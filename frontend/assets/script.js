00f8ed7091db1eda92a6ae3ce28cf095124a829e
fernando-rodrigues-pinto <fernandopinto.frp@gmail.com>
2025-03-27 20:42:38 -0300

    fix: arrumando script

--- frontend/assets/script.js
+++ frontend/assets/script.js
@@ -46,33 +46,35 @@ $(document).ready(function () {
 
     // RESPONSIVIDADE
     function adjustDivForMobile() {
-        if ($(window).width() <= 768) { // Defina o tamanho para considerar 'mobile'
+        if ($(window).width() <= 1024) { // Defina o tamanho para considerar 'mobile'
             $('.ocultar-mobile').removeClass('d-flex').hide();
-            $('#sobre-main').removeClass('ms-5 px-5').addClass('text-center');
+            $('#sobre-main').removeClass('ms-5 px-5').addClass('text-center d-flex flex-column justify-content-center align-items-center');
             $('.text-secondary').removeClass('ms-5 px-5').addClass('text-center');
             $('#sobre').addClass('align-items-center');
             $('#mvv').removeClass('flex-row').addClass('flex-column align-items-center');
-            $('#mvv-div').removeClass('row');
+            $('#mvv-div').removeClass('row').addClass('d-flex flex-column align-items-center');
+            $('#contato-main').removeClass('me-3 margin-left').addClass('text-center d-flex flex-column justify-content-center align-items-center');
+            $('.contato-info').addClass('justify-content-center')
 
-            $('#equipe-desktop').hide();
+            $('#equipe-desktop').removeClass('d-flex').hide();
         } else {
             $('.ocultar-mobile').addClass('d-flex').show();
-            $('#sobre-main').addClass('ms-5 px-5').removeClass('text-center');
+            $('#sobre-main').addClass('ms-5 px-5').removeClass('text-center d-flex flex-column justify-content-center align-items-center');
             $('.text-secondary').addClass('ms-5 px-5').removeClass('text-center');
             $('#sobre').removeClass('align-items-center');
             $('#mvv').addClass('flex-row').removeClass('flex-column align-items-center');
-            $('#mvv-div').addClass('row');
+            $('#mvv-div').addClass('row').removeClass('d-flex flex-column align-items-center');
+            $('#contato-main').addClass('me-3 margin-left').removeClass('text-center d-flex flex-column justify-content-center align-items-center');
+            $('.contato-info').removeClass('justify-content-center')
 
-            $('#equipe-desktop').show();
+            $('#equipe-desktop').addClass('d-flex').show();
         }
     }
 
     // Chamar a função no carregamento da página e quando a janela for redimensionada
     adjustDivForMobile();
     $(window).resize(adjustDivForMobile);
-
-
-    //Quando usuário aperta espaço pula pro sobrenome
+    
     let inputNome = $('#input-nome');
     let inputSobrenome = $('#input-sobrenome');
     let text = '';
