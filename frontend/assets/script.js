b9402d1180ef2c9f8bc1b059e762d6c95f3af6f7
fernando-rodrigues-pinto <fernandopinto.frp@gmail.com>
2025-03-26 22:52:44 -0300

    style: começo da responsividade do site

--- frontend/assets/script.js
+++ frontend/assets/script.js
@@ -6,14 +6,17 @@ $(document).ready(function () {
         var navbar = $('.navbar');
         var navbarBrand = $('.navbar-brand');
         var navbarNav = $('.navbar-nav');
+        var navbarToggler = $('.navbar-toggler-icon')
 
         if (scrollPosition >= homeSection && scrollPosition < $('#sobre').offset().top) {
             navbar.removeClass('bg-dark').addClass('bg-transparent');
             navbarBrand.addClass('d-none');
+            navbarToggler.addClass('d-none');
             navbarNav.addClass('me-auto');
         } else {
             navbar.removeClass('bg-transparent').addClass('bg-dark');
             navbarBrand.removeClass('d-none');
+            navbarToggler.removeClass('d-none');
             navbarNav.removeClass('me-auto');
         }
     }
@@ -40,4 +43,31 @@ $(document).ready(function () {
 
     updateActiveSection();
     $(window).on('scroll', updateActiveSection);
+
+    // RESPONSIVIDADE
+    function adjustDivForMobile() {
+        if ($(window).width() <= 768) { // Defina o tamanho para considerar 'mobile'
+            $('.ocultar-mobile').removeClass('d-flex').hide();
+            $('#sobre-main').removeClass('ms-5 px-5').addClass('text-center');
+            $('.text-secondary').removeClass('ms-5 px-5').addClass('text-center');
+            $('#sobre').addClass('align-items-center');
+            $('#mvv').removeClass('flex-row').addClass('flex-column align-items-center');
+            $('#mvv-div').removeClass('row');
+
+            $('#equipe-desktop').hide();
+        } else{
+            $('.ocultar-mobile').addClass('d-flex').show();
+            $('#sobre-main').addClass('ms-5 px-5').removeClass('text-center');
+            $('.text-secondary').addClass('ms-5 px-5').removeClass('text-center');
+            $('#sobre').removeClass('align-items-center');
+            $('#mvv').addClass('flex-row').removeClass('flex-column align-items-center');
+            $('#mvv-div').addClass('row');
+
+            $('#equipe-desktop').show();
+        }
+    }
+    
+    // Chamar a função no carregamento da página e quando a janela for redimensionada
+    adjustDivForMobile();
+    $(window).resize(adjustDivForMobile);
 });
