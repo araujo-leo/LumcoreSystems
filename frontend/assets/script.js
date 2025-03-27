c6e26d9f0956716ebc8ab241aa43c532f32a4325
fernando-rodrigues-pinto <fernandopinto.frp@gmail.com>
2025-03-26 21:44:11 -0300

    style: protótipo final para telas grandes

--- frontend/assets/script.js
+++ frontend/assets/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // FUNÇÃO DE ATUALIZAR A NAVBAR AO SAIR DO HOME
     function updateNavbar() {
         var scrollPosition = $(window).scrollTop();
         var homeSection = $('#home').offset().top;
@@ -19,4 +20,24 @@ $(document).ready(function () {
 
     updateNavbar();
     $(window).on('scroll', updateNavbar);
+
+    // FUNÇÃO PARA CHECAR E ATUALIZAR SEÇÃO ATUAL DA PÁGINA
+    function updateActiveSection() {
+        let scrollPosition = $(window).scrollTop();
+        let offset = $(window).height() / 3; // Ajuste para ativar antes da seção ocupar a tela inteira
+
+        $('section').each(function () {
+            let sectionTop = $(this).offset().top - offset;
+            let sectionBottom = sectionTop + $(this).outerHeight();
+            let sectionId = $(this).attr('id');
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                $('.nav-link').removeClass('active');
+                $(`.nav-link[href="#${sectionId}"]`).addClass('active');
+            }
+        });
+    }
+
+    updateActiveSection();
+    $(window).on('scroll', updateActiveSection);
 });
