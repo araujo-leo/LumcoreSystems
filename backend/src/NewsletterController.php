570ac9d7bed97fe7ca558cba706d52bd7e130853
araujo-leo <leoaraujofnd@gmail.com>
2025-03-30 11:04:29 -0300

    fix: botão clicavel no email para exclusao do newsletter

--- backend/src/NewsletterController.php
+++ backend/src/NewsletterController.php
@@ -52,7 +52,7 @@ class NewsletterController
                 'header' => 'Novidades da nossa Newsletter',
                 'message' => 'Seja bem-vindo ao canal de notícias da Lumcore Systems.',
                 'company' => 'Lumcore Systems',
-                'unsubscribe_button' => '<hr><a href="localhost:5500/?unsubscribe=' . $email . '">Cancelar inscrição</a>' 
+                'unsubscribe_button' => '<hr><a href="http://localhost:5500/?unsubscribe=' . $email . '">Cancelar inscrição</a>'
             ];
 
             $mailer = new Mailer();
